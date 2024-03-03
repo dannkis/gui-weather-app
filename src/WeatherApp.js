58f78e57a2def03cfc63d3c5caf609a7e61f9842
Daniil Hristoforov Zhelyazkov <d.zhelyazkov@se22.qmul.ac.uk>
2024-03-03 00:12:04 +0000

    nav basic structure

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./WeatherApp.scss"
+import "./customReact.scss";
+import "./WeatherApp.scss";
+import Nav from "./components/nav/nav.js";
+import Home from "./pages/home/home.js";
+import Blog from "./pages/blog/blog.js";
+import FAQ from "./pages/frequently_asked_questions/frequentlyAskedQuestions.js";
 
 function App() {
   return (
-    <Router>
-
-    </Router>
+    <div className="container-fluid">
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/faq" component={FAQ} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import "./nav.scss";
 
 export default function nav() {
   return (
-    <div className='row'>nav</div>
-  )
+      <Navbar expand="lg" className="px-5">
+        <Navbar.Brand href="">React-Bootstrap</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-end flex-grow-1">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/blog">
+              Blog
+            </Nav.Link>
+            <Nav.Link as={Link} to="/faq">
+              FAQ
+            </Nav.Link>
+            <NavDropdown title="Contact Us" id="basic-nav-dropdown">
+              <NavDropdown.Item href="mailto:" className="text-center">
+                Email
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item
+                href="https://www.linkedin.com/in/daniil-zhelyazkov"
+                className="text-center"
+              >
+                Phone
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+  );
 }
