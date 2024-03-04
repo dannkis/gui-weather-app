b065d53b326388b532245142961c8f58ae2c1c18
Daniil Hristoforov Zhelyazkov <d.zhelyazkov@se22.qmul.ac.uk>
2024-03-04 20:23:18 +0000

    font and logo

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,40 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logo_main from "../../media/logos/logo_main.png";
 import "./nav.scss";
 
 export default function nav() {
   return (
-      <Navbar expand="lg" className="px-5">
-        <Navbar.Brand href="">React-Bootstrap</Navbar.Brand>
+    <Navbar expand="lg" className="px-5">
+      <Navbar.Brand as={Link} to="/">
+        <img className="img-fluid" src={logo_main}></img>
+      </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="justify-content-end flex-grow-1">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/blog">
-              Blog
-            </Nav.Link>
-            <Nav.Link as={Link} to="/faq">
-              FAQ
-            </Nav.Link>
-            <NavDropdown title="Contact Us" id="basic-nav-dropdown">
-              <NavDropdown.Item href="mailto:" className="text-center">
-                Email
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                href="https://www.linkedin.com/in/daniil-zhelyazkov"
-                className="text-center"
-              >
-                Phone
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="justify-content-end flex-grow-1">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/blog">
+            Blog
+          </Nav.Link>
+          <Nav.Link as={Link} to="/faq">
+            FAQ
+          </Nav.Link>
+          <NavDropdown title="Contact Us" id="basic-nav-dropdown">
+            <NavDropdown.Item href="mailto:" className="text-center">
+              Email
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+              href="https://www.linkedin.com/in/daniil-zhelyazkov"
+              className="text-center"
+            >
+              Phone
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
