16a70a92ca394a1dd52419e563049ae057486085
kenza1605 <ec22965@qmul.ac.uk>
2024-03-18 20:56:57 +0000

    FAQs

--- src/pages/frequently_asked_questions/faqs_pages/question2.js
+++ src/pages/frequently_asked_questions/faqs_pages/question2.js
@@ -9,20 +9,22 @@ export default function question2() {
         </div>
       </div>
       <div className="row">
-        <div className="col col-sm-8 offset-sm-2 d-column justify-content-center border-30 border-dark-tr text-center p-4 bg-success">
-          <h3><strong>Lorem ipsum dolor sit amet consectetur?</strong></h3>
+      <div className="col col-sm-8 offset-sm-2 d-column justify-content-center border-30 border-dark-tr text-center p-4 bg-success">
+          <h3><strong>What features are available for free, and what features require a subscription?</strong></h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur. Feugiat tellus lectus dolor
-            leo. Quis amet blandit viverra neque at. Iaculis varius suscipit
-            morbi faucibus rutrum.
+          The basic features of the app, including current weather updates and limited forecasts, are available for free. Premium features, such as extended forecasts, crop-specific insights, and ad-free experience, require a subscription.
           </p>
         </div>
         <div className="col col-sm-8 offset-sm-2 d-column justify-content-center border-30 border-dark-tr text-center p-4 mt-4">
-          <h3><strong>Lorem ipsum dolor sit amet consectetur?</strong></h3>
+          <h3><strong>How do I subscribe to a premium plan?</strong></h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur. Feugiat tellus lectus dolor
-            leo. Quis amet blandit viverra neque at. Iaculis varius suscipit
-            morbi faucibus rutrum.
+          To subscribe to a premium plan, go to the "Subscription" section within the app and select the desired plan. Follow the prompts to complete the subscription process.
+          </p>
+        </div>
+        <div className="col col-sm-8 offset-sm-2 d-column justify-content-center border-30 border-dark-tr text-center p-4 mt-4 bg-success">
+          <h3><strong>Can I change or cancel my subscription at any time?</strong></h3>
+          <p>
+          Yes, you can manage your subscription settings, including changing or canceling your subscription, at any time through the app's settings or your device's app store.
           </p>
         </div>
       </div>
